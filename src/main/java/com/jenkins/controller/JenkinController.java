bc90c4dc6d6f9446e777c19d95566e9bb02ec0a6
Kaung Htet Zaw <zawk29006@gmail.com>
2025-11-13 20:18:48 +0630

    Add JenkinController

---
+++ src/main/java/com/jenkins/controller/JenkinController.java
@@ -0,0 +1,19 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class JenkinController {
+    @GetMapping("/jenkins")
+    public String create() {
+        return "Jenkins Testing";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Jenkins Testing App";
+    }
+}
