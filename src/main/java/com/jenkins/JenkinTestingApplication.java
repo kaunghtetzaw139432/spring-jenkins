9c700627b766d56a34c332ea6a33f1432c31cc36
Kaung Htet Zaw <zawk29006@gmail.com>
2025-11-13 17:43:17 +0630

    jenkins testing

---
+++ src/main/java/com/jenkins/JenkinTestingApplication.java
@@ -0,0 +1,24 @@
+package com.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinTestingApplication {
+	public static Logger logger = LoggerFactory.getLogger(JenkinTestingApplication.class);
+
+	@PostConstruct
+	public void intt() {
+		logger.info("Application started....");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed");
+		SpringApplication.run(JenkinTestingApplication.class, args);
+	}
+
+}
